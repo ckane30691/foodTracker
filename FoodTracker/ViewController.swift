e9ca17febe85d026cdd7ea9f84b6d11a60d27638
Cory Kane <ckane30691@gmail.com>
2018-01-11 15:21:28 -0800

    Implement ImagePickerControllerDidCancel & imagePickerController

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -50,5 +50,24 @@ class ViewController: UIViewController, UITextFieldDelegate,
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user cancelled
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        // The info dictionary may contain multiple representations of the image.  We want to use only the original
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as?
+            UIImage else {
+                fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker
+        dismiss(animated: true, completion: nil)
+    }
 }
 
