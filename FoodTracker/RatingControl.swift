719e9bbc415e40e7f993aea1a8bb9afc28abf5bc
Cory Kane <ckane30691@gmail.com>
2018-01-13 20:38:42 -0800

    add UIStackView Class and init placeholders

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Cory Kane on 1/13/18.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    // MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
