81f89531d638efeca8e00332a8901d5853d0fd4b
Cory Kane <ckane30691@gmail.com>
2018-01-16 21:05:06 -0800

    Implement setupButtons function in RatingControl Class

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -12,10 +12,28 @@ class RatingControl: UIStackView {
     // MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    // MARK: Private Methods
+    
+    private func setupButtons() {
+        // create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add Button to the stack
+        addArrangedSubview(button)
     }
 
 }
