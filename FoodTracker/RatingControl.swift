071628c0ba91da051a9344a704ec5f0f87660a18
Cory Kane <ckane30691@gmail.com>
2018-01-25 18:06:40 -0800

    Clear out existing buttons if there are any in setupButtons function

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -42,6 +42,13 @@ import UIKit
     // MARK: Private Methods
     
     private func setupButtons() {
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
         for _ in 0..<starCount {
             // Create the button
             let button = UIButton()
